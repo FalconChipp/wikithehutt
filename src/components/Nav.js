0bcef6a576c0613cdd16600b911d769d29c0a328
FalconChipp <112320693+FalconChipp@users.noreply.github.com>
2022-10-19 17:15:34 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
-import { Link, useStaticQuery } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 
 
 export default function Navbar({ data }) {
-  const LogoSettings = useStaticQuery( `
+  const LogoSettings = useStaticQuery(graphql `
   fragment LogoSettings on File {
     childImageSharp {
       gatsbyImageData(
@@ -16,7 +16,7 @@ export default function Navbar({ data }) {
   }
 `)
 
-const Logo = useStaticQuery(`
+const Logo = useStaticQuery(graphql `
   query {
     Logo: file(relativePath: {eq: "Wiki-The-Hutt-Logo.png"}) {
       ...LogoSettings
@@ -26,7 +26,7 @@ const Logo = useStaticQuery(`
   return (
         <nav className="mt-[35px] bg-transparent mb-[35px] mobile:hidden">
             <div className="font-jedi container-2xl text-2xl text-center text-white">
-                <GatsbyImage image={data.Logo} alt="Logo" />
+                <GatsbyImage image={data.Logo.childImageSharp.gatsbyImageData} alt="Logo" />
                 <Link className="m-5 pt-1 pb-2 px-3 hover:shadow-redSaber hover:text-redSaber rounded-xl" to="/">Home</Link>
                 <Link className="m-5 pt-1 pb-2 px-3 hover:shadow-blueSaber hover:text-blueSaber rounded-xl" to="/characters">Characters</Link>
                 <Link className="m-5 pt-1 pb-2 px-3 hover:shadow-greenSaber hover:text-greenSaber rounded-xl" to="/weapons">Weapons</Link>
