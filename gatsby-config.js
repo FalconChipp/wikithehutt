3263c14a42d4345d50af6c8334d8a423584bcc76
FalconChipp <112320693+FalconChipp@users.noreply.github.com>
2022-10-26 17:25:13 +0100

    installed netlify plugin
    npm install gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@
       `gatsby-plugin-image`,
       `gatsby-plugin-sharp`,
       `gatsby-transformer-sharp`, // Needed for dynamic images
+      `gatsby-plugin-netlify`,
       {
         resolve: `gatsby-source-filesystem`,
         options: {
