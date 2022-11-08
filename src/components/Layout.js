41e1a5311a59564cdbfc30255f8a0cd0ff00bbb8
Ryan <112320693+FalconChipp@users.noreply.github.com>
2022-11-08 15:29:59 +0000

    Removed utils and added hamburger to the nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Navbar from '../components/Nav'
 import Header from "./Header";
 import Footernew from "./FooterNew";
+import Navbar from "./Nav";
+import Hamburger from "./hamburger";
 
 export default function Layout({ children }) {
     return (
         <div className="layout">
             <Header />
             <Navbar />
+            {/* <DesktopNavMenu/>
+            <MobileNavMenu /> */}
             <div className="content">
                 {children}
             </div>
