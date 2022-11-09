2df8766648002c41179374cef13367fad3fb4ae7
Ryan <112320693+FalconChipp@users.noreply.github.com>
2022-11-09 16:35:52 +0000

    Added logo to mobile nav & moved bg img

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Header from "./Header";
 import Footernew from "./FooterNew";
 import Navbar from "./Nav";
 import Hamburger from "./hamburger";
 
 export default function Layout({ children }) {
     return (
-        <div className="layout">
-            <Header />
+        
+        <div className="bg-site w-full h-full bg-contain bg-repeat-y">
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
             <Navbar />
             {/* <DesktopNavMenu/>
             <MobileNavMenu /> */}
