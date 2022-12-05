787f672f64f9e04e28f1ac8465321c7be6fefa31
Ryan <112320693+FalconChipp@users.noreply.github.com>
2022-12-05 11:10:40 +0000

    Code Clean, image optimising & background image timeline test

--- src/pages/weapons.js
+++ src/pages/weapons.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { graphql, Link } from "gatsby";
 
 export const Head = () => <title>Weapons</title>
